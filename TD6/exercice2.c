69491754a16879bc775a006aa22ff6fadd804355
theophile <theomolinatti@gmail.com>
2020-03-10 13:42:39 +0100

    Modification avec la fonction times et non time

--- TD6/exercice2.c
+++ TD6/exercice2.c
@@ -4,29 +4,27 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <time.h>
+#include <sys/times.h>
 
-void aff_rep(char *path)
+void aff_rep(char *path, struct tms buf)
 {
+	sleep(2);
+	times(&buf);
 	execlp("ls", "ls", path, "-R", NULL);
 }
 
 int main(int argc, char **argv)
 {
-	time_t timeBefore = time(NULL);
+	struct tms buf;
+	int status;
 	
 	if(!fork())
-	{
-		sleep(2);
-		aff_rep("/home/user");
-		exit(0);
-	}
-
+		aff_rep("/home/user", buf);
 	
-	wait(NULL);
+	wait(&status);
 	
-	time_t timeAfter = time(NULL);
-	printf("Temps d'excution : %ld\n", timeAfter-timeBefore);
+	times(&buf);
+	printf("Temps d'excution : %f\n", (float)( (buf.tms_cutime + buf.tms_cstime) / (sysconf(_SC_CLK_TCK) )));
 
 	exit(0);
 }
