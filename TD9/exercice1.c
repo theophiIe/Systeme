5deb7102ffd7af26cc50fd7fcd1d4dc4c617a422
theophile <theomolinatti@gmail.com>
2020-04-13 11:57:35 +0200

    Question 1.1 fini

--- TD9/exercice1.c
+++ TD9/exercice1.c
@@ -2,7 +2,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void *print_string(void *arg)
+{
+	char *str = (char *) arg;
+	
+	printf("%s\n", str);
+	return NULL;
+}
+
 int main()
 {
+	pthread_t tid;
 	
+	pthread_create(&tid, NULL, print_string, "Hello world!");
+	pthread_join(tid, NULL);
 }
