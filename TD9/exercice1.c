6134d74747fedb1a9196a65f1c225ae82f5f75d8
theophile <theomolinatti@gmail.com>
2020-04-13 12:48:04 +0200

    Question 1.2 terminé

--- TD9/exercice1.c
+++ TD9/exercice1.c
@@ -1,13 +1,24 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 void *print_string(void *arg)
 {
 	char *str = (char *) arg;
 	
-	printf("%s\n", str);
-	return NULL;
+	printf("message : %s\n", str);
+	pthread_exit( NULL );
+}
+
+void *print_alea_int(void *arg)
+{
+	int *alea = (int *) arg;
+	
+	printf("alea int : %d\n", *alea);
+	pthread_exit( NULL );
 }
 
 void gestion_thread(void *(*start_routine) (void *), void *arg)
@@ -20,7 +31,14 @@ void gestion_thread(void *(*start_routine) (void *), void *arg)
 
 int main()
 {
+	//Question 1.1
 	gestion_thread(print_string, "Hello world!");
+	
+	//Question 1.2
+	srand(getpid());
+	
+	int alea = rand() % 100;
+	gestion_thread(print_alea_int,  &alea);
 	
 	return 0;
 }
